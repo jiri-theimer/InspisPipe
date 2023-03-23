2d9f1a95cd35dc8583bfb12122f4f2de8f34dd4d
jiri-theimer <jiri.theimer@gmail.com>
2023-03-23 11:08:05 +0100

    a

--- bas/GinisHelper.cs
+++ bas/GinisHelper.cs
@@ -50,6 +50,7 @@ public class GinisHelper
         {
             case "lamos":
             case "hovado":
+            case "robot":
             case "kuchar":
                 {
                     this.USERNAME = "inspis";    // vyjímka
